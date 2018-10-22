f8017625e07d8004edeef711dab47cbdea668391
yangpeihao <1521567538@qq.com>
2018-10-22 23:24:10 +0800

    再次把本地工程提交到Git中

---
+++ Hello World/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        test();
+        System.out.println("Hello World!");
+    }
+
+    public static void test(){
+        System.out.println("test..........");
+    }
+}
